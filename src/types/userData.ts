8982f6d86ca381a81cea69c6f877a1d29edf26c6
kusira <takaaki200302@gmail.com>
2024-08-04 12:43:55 +0900

    🔧 userDataの型宣言を書き直しました。

--- src/types/userData.ts
+++ src/types/userData.ts
@@ -8,11 +8,35 @@ export type UserData = {
   user_info?: {
     birthday?: string;
     gender?: string;
-    imageUrl?: string;
+    image_url?: string;
     name?: string;
     selected_goal?: string;
     selected_residential?: string;
     selected_work?: string;
     self_introduction?: string;
+  };
+  app_info?: {
+    membership_status?: 'free' | 'premium';
+    like_to?: {
+      [key: string]: string[];
+    };
+    like_from?: {
+      [key: string]: string[];
+    };
+    talk_list?: {
+      [key: string]: {
+        messages?: {
+          people?: string;
+          text?: string;
+        }[];
+      };
+    };
+  };
+  current_info?: {
+    peopleCount?: string;
+    isGPS?: boolean;
+    longitude?: number;
+    latitude?: number;
+    winZone?: string;
   };
 };
